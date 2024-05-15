613f738c9d1057a210a57f11a61a73508c5d0fd7
Dylan Gilley <dylan.gilley@gmail.com>
2024-05-15 11:14:39 -0400

    Added command line option for running verbose.

--- runSDL.py
+++ runSDL.py
@@ -10,6 +10,7 @@ def main(argv):
     # Parse the command line
     parser = argparse.ArgumentParser()
     parser.add_argument(dest='input_file', type=str)
+    parser.add_argument('--verbose', dest='verbose', action='store_true', default=False)
     parser.add_argument('--MAE', dest='MAE', action='store_true', default=True)
     parser.add_argument('--noMAE', dest='noMAE', action='store_true', default=False)
     args = parser.parse_args()
@@ -21,7 +22,7 @@ def main(argv):
 
     # Run each requested campaign
     for campaign in campaign_list:
-        campaign.run()
+        campaign.run(verbose=args.verbose)
 
         # If MAE is requested, read the data back in from the output file and calculate the MAE
         if args.MAE:
